6c11d5ed2820e106f8575ed4c99ec476b057bbd3
Jeffrey Wu <lwa131@sfu.ca>
2025-03-25 14:48:57 -0700

    home page draft

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- add tab icon-->
+    <link rel="icon" href="img/logo.webp" type="image/x-icon">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home</title>
+</head>
+
+</html>
