8e7d32cc439c13ea51c7001342391de83d9d9897
Jeffrey Wu <lwa131@sfu.ca>
2025-03-25 14:57:26 -0700

    Add the icon image inside the img folder

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- add tab icon-->
-    <link rel="icon" href="img/logo.webp" type="image/x-icon">
+    <link rel="icon" href="img/icon.webp" type="image/x-icon">
 
     <link rel="stylesheet" href="css/style.css">
     <title>Home</title>
