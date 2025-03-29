3286bed4ef8fcbdb8394ee99d7aa1774ef1aa741
Jeffrey Wu <lwa131@sfu.ca>
2025-03-29 15:54:54 -0700

    Update index.php

--- index.php
+++ index.php
@@ -12,4 +12,19 @@
     <title>Home</title>
 </head>
 
+<body>
+    <header>
+        <!-- Nav bar -->
+        <nav class="grids-nav nav-main" aria-label="Main Navigation">
+            <a href="index.html" class="nav-logo"><img src="img/icon.webp" width="81" height="51"
+                    alt="Logo link to Home page"></a>
+
+            <a href="#about" class="nav-button">ABOUT</a>
+
+            <a href="#portfolio" class="nav-button">PORTFOLIO</a>
+
+            <a href="#contact" class="nav-button">CONTACT</a>
+        </nav>
+    </header>
+
 </html>
