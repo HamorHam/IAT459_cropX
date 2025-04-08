fd683e1a0f932049d4cefa1e5af7a225f7d7b020
Jeffrey Wu <lwa131@sfu.ca>
2025-04-07 20:55:29 -0700

    Dashboard link to the commented plants

--- cropX/public/member/index.php
+++ cropX/public/member/index.php
@@ -118,6 +118,11 @@ include(SHARED_PATH . '/member_header.php');
   <form style="margin-bottom:3em" action="index.php" method="post" onsubmit="return confirm('Are you sure you want to delete your account? This action cannot be undone.');">
     <button type="submit" name="delete_account">Delete Account</button>
   </form>
+
+  <h3 style="margin-top:2em">My Comments</h3>
+<form style="margin-bottom:1em" action="<?php echo url_for('/member/commented_plant.php'); ?>" method="get">
+  <button type="submit">Commented Plants</button>
+</form>
 </div>
 
 <?php include(SHARED_PATH . '/member_footer.php'); ?>
